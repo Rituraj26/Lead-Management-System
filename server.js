ddd3eda1caab8dd7520e77f9f87ecdf91ba7b225
Rituraj <riturajguwahati26@gmail.com>
2020-11-01 00:20:55 +0530

    basic drawer implemented

--- server.js
+++ server.js
@@ -17,6 +17,16 @@ app.use('/leads', leads);
 
 app.use(errorHandler);
 
+// Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+    // Set static folder
+    app.use(express.static('client/build'));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+}
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
